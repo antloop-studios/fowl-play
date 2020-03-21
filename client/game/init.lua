c3358bfebbf45fd2489a2bb0cd7ed87baed3166c
nilq <i.am.niels.nielsen@gmail.com>
2020-03-21 20:33:26 +0100

    Add baton and bump

--- client/game/init.lua
+++ client/game/init.lua
@@ -1,18 +1,31 @@
-local game = {}
+local game = {
+    entities = {}
+}
 
 function game:enter()
     self.camera = libs.camera(0, 0)
+    self.world  = libs.bump.newWorld(64)
+    self.input  = libs.baton.new {
+        controls = {
+            left  = {'key:a', 'axis:leftx-', 'button:dpleft' },
+            right = {'key:d', 'axis:leftx+', 'button:dpright'},
+            up    = {'key:w', 'axis:lefty-', 'button:dpup'   },
+            down  = {'key:s', 'axis:lefty+', 'button:dpdown' },
+        },
+        pairs = {
+            move = {'left', 'right', 'up', 'down'}
+        },
+        joystick = love.joystick.getJoysticks()[1],
+    }
 end
 
 function game:update(dt)
-    self.camera:rotate(dt)
+    self.input:update()
 end
 
 function game:draw()
     self.camera:attach()
 
-    love.graphics.setColor(1, 1, 0)
-    love.graphics.rectangle("fill", -50, -50, 100, 100)
 
     self.camera:detach()
 end
--- client/libs/init.lua
+++ client/libs/init.lua
@@ -2,9 +2,13 @@ local path = "libs/"
 
 local camera    = require(path .. "camera")
 local gamestate = require(path .. "gamestate")
+local bump      = require(path .. "bump")
+local baton     = require(path .. "baton")
 
 
 return {
     camera    = camera,
-    gamestate = gamestate
+    gamestate = gamestate,
+    bump      = bump,
+    baton     = baton,
 }
