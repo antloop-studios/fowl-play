67ce1b360458d4abc0b9d5e1e84d98334168524d
Daniels Kursits <daniels.kursits@gmail.com>
2020-03-22 22:23:38 +0200

    ping ping

--- client/game/messages/connect.lua
+++ client/game/messages/connect.lua
@@ -3,16 +3,17 @@ return function(self, event)
     self.uid = event.uid
 
     for i, entity in pairs(event.entities) do
-        local id
+        local id, ping
         if i ~= self.uid then
             local color = entity.sprite.color
             entity.sprite = {name = "player", color = color, scale = 1}
+            ping = entity.ping
             entity.ping = nil
             id = e.player(entity)
         end
-        self.entities[i] = {e = e.get(id), id = id}
+        self.entities[i] = {e = e.get(id), id = id, ping = ping}
     end
 
-    game:send_log("[server] player joined the game", { 0, 1, 0})
+    game:send_log("[server] player joined the game", {0, 1, 0})
     self.teams = event.teams
 end
