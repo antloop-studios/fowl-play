6548f6c253b7165dc9525447cee4f3c84c9056e3
nilq <i.am.niels.nielsen@gmail.com>
2020-03-22 22:20:34 +0100

    Fix background color

--- client/main.lua
+++ client/main.lua
@@ -6,7 +6,7 @@ libs = require "libs"
 local gamestate = libs.gamestate
 local game = require "game"
 
-love.graphics.setBackgroundColor(23 / 200, 67 / 200, 84 / 200)
+love.graphics.setBackgroundColor(27.8 / 200, 17.6 / 200, 23.5 / 200)
 
 config = {server = "fowl2.antloop.world:5700"}
 
